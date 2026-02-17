f371ed4ecb1deccc265dfb433ae9f3dcb44a59e8
Greison <greisonboff@gmail.com>
2026-02-17 15:58:35 -0300

    fix jwt

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function generateToken(userId) {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: "1d",
   });
 }
 
