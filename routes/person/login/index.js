a65316544f3740ce29e0027756339c60708e692b
Greison <greisonboff@gmail.com>
2026-02-17 17:07:59 -0300

    fix sameSite

--- routes/person/login/index.js
+++ routes/person/login/index.js
@@ -6,21 +6,34 @@ const Person = require("../../../models/Person");
 const { generateToken } = require("../../../utils/generateToken");
 
 //login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    if (!email) return res.status(422).json({ isValid: false, error: 'email is required' });
+    if (!email)
+      return res
+        .status(422)
+        .json({ isValid: false, error: "email is required" });
 
-    if (!password) return res.status(422).json({ isValid: false, error: 'password is required' });
+    if (!password)
+      return res
+        .status(422)
+        .json({ isValid: false, error: "password is required" });
 
     const user = await Person.findOne({ email });
 
-    if (!user || !user.password) return res.status(400).json({ isValid: false, error: 'Usuário não encontrado' });
+    if (!user || !user.password)
+      return res
+        .status(400)
+        .json({ isValid: false, error: "Usuário não encontrado" });
 
-    const valid = await bcrypt.compare(password?.toString(), user?.password.toString());
+    const valid = await bcrypt.compare(
+      password?.toString(),
+      user?.password.toString(),
+    );
 
-    if (!valid) return res.status(401).json({ isValid: false, error: 'Senha inválida' });
+    if (!valid)
+      return res.status(401).json({ isValid: false, error: "Senha inválida" });
 
     const token = generateToken(user._id);
 
@@ -28,7 +41,7 @@ router.post('/login', async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // HTTPS em produção
-        sameSite: "strict", // ajuda na proteção CSRF
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000, // 1 dia
       })
       .status(200)
@@ -39,13 +52,11 @@ router.post('/login', async (req, res) => {
           name: user.name,
           email: user.email,
           img: user.img,
-          avatarColar: user.avatarColar
+          avatarColar: user.avatarColar,
         },
       });
-
-
   } catch (error) {
-    res.status(500).json({ error: error ? error : 'Internal server error' });
+    res.status(500).json({ error: error ? error : "Internal server error" });
   }
 });
 
