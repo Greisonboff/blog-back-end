ed7198f33b48017c2c9d208495b686415455b72e
Greison Nicolau Böff <greisonboff@hotmail.com>
2026-05-13 23:37:13 -0300

    fix: user data get all

--- routes/person/getAll/index.js
+++ routes/person/getAll/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const Person = require("../../../models/Person");
 // get all users
 router.get("/", async (req, res) => {
   try {
-    const people = await Person.find();
+    const people = await Person.find().populate("name", "img", "email");
     res.status(200).json(people);
   } catch (error) {
     res.status(500).json({ error: error });
